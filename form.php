2070ee43fd914042ee963e18f5e17ab8191f33d2
ky-ai <73719477+ky-ai@users.noreply.github.com>
2020-11-21 23:18:21 -0600

    Add files via upload

--- form.php
+++ form.php
@@ -1,117 +1,28 @@
-<?php
-
-/*use PHPMailer\PHPMailer\PHPMailer;
-require __DIR__ . '/vendor/autoload.php';
-
-$errors = [];
-$errorMessage = '';
-
-if (!empty($_POST)) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    if (empty($name)) {
-        $errors[] = 'Name is empty';
-    }
-
-    if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
-    }
-
-    if (empty($message)) {
-        $errors[] = 'Message is empty';
-    }
-
-
-    if (!empty($errors)) {
-        $allErrors = join('<br/>', $errors);
-        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
-    } else {
-        $mail = new PHPMailer();
-
-        // specify SMTP credentials
-        $mail->isSMTP();
-        $mail->Host = 'smtp.mailtrap.io';
-        $mail->SMTPAuth = true;
-        $mail->Username = '4a53d3179c01a6';
-        $mail->Password = '6f15f802eb90c0';
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 2525;
-
-        $mail->setFrom($email, 'Mailtrap Website');
-        $mail->addAddress('piotr@mailtrap.io', 'Me');
-        $mail->Subject = 'New message from your website';
-
-        // Enable HTML if needed
-        $mail->isHTML(true);
-
-        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", nl2br($message)];
-        $body = join('<br />', $bodyParagraphs);
-        $mail->Body = $body;
-
-        echo $body;
-        if($mail->send()){
-
-            header('Location: thank-you.html'); // redirect to 'thank you' page
-        } else {
-            $errorMessage = 'Oops, something went wrong. Mailer Error: ' . $mail->ErrorInfo;
-        }
-    }
-
-} */?>
-<?php
-if (!empty($errors)) {
-   $allErrors = join('<br/>', $errors);
-   $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
-}?>
-
-
-<?php
-
-$errors = [];
-$errorMessage = '';
-
-if (!empty($_POST)) {
-    $name = $_POST['Name'];
-    $email = $_POST['Email'];
-    $message = $_POST['Message'];
-
-    if (empty($name)) {
-        $errors[] = 'Name is empty';
-    }
-
-    if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
-    }
-
-    if (empty($message)) {
-        $errors[] = 'Message is empty';
-    }
-
-
-    if (empty($errors)) {
-        $toEmail = 'systemtest081588@gmail.com';
-        $emailSubject = 'New email from your contact form';
-        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
-
-        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
-        $body = join(PHP_EOL, $bodyParagraphs);
-
-        if (mail($toEmail, $emailSubject, $body, $headers)) {
-            header('Location: thank-you.html');
-        } else {
-            $errorMessage = 'Oops, something went wrong. Please try again later';
-        }
-    } else {
-        $allErrors = join('<br/>', $errors);
-        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
-    }
-}
-
-?>
-<?php echo((!empty($errorMessage)) ? $errorMessage : '') ?>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>About</title>
+    <link href="https://fonts.googleapis.com/css2?family=Goldman&display=swap" rel="stylesheet">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+          <form method="POST" action="send.php" id="contact-form">
+                <div class="contact-information-wrapper">
+                    <fieldset>
+                        <legend> Contact Details</legend>
+                        <label>Name: <br />
+                        <input type="text" name="name" value="dima"/></label> <br/>
+                        <label>Email: <br />
+                        <input style = "cursor: pointer;" type="text" name="email" value="dima@dima.com" /></label> <br/>
+                        <textarea name="message" cols="20" rows="10">
+                            Enter your text
+                        </textarea>
+                        <input type="submit" value="send" /></p>
+                    </fieldset>
+                </div>
+    
+</body>
+</html>
